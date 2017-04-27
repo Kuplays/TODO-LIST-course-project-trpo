8cb14a61bd1f7984ebbe7b27d08a6892294ebdc4
Aleksandr Kuplays <alexkuplays@gmail.com>
2017-04-27 21:21:10 +0700

    Changed function types

--- includes/task.h
+++ includes/task.h
@@ -2,16 +2,16 @@
 #define TASK_H
 struct Task {
     int id;
-    char tTitle[40]; //probably enough, heh?
+    char tTitle[30]; //probably enough, heh?
     char tInfo[100]; //do we need more?
-    char tAcceptedDate[80]; //probably change later
+    char tAcceptedDate[30]; //probably change later
     int isImportant; //1 means important, 0 not
     int isInProgress; //1 means done 0 in progress. Probably won`t need this, but who knows
-};
+} task;
 
 //function prototypes goes here
-void newTask(FILE *);
+int newFile(FILE *);
 void printAllTasks(FILE *);
 void showTask(FILE *, int); //probably by id?
-void removeTask(FILE *, int);//same as above?
+int removeTask(FILE *, int);//same as above?
 #endif
