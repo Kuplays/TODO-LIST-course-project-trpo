688993eaad863fc11ba58cdaa21151039a1f66ec
crasher746 <Crasher746@gmail.com>
2017-05-08 01:50:05 +0700

    Change menu function

--- src/task.c
+++ src/task.c
@@ -67,44 +67,45 @@ int menu ()
     int variant;
     printf("1. Создать новый файл \n");
     printf("2. Вывести все задачи \n");
-    printf("3. Выход \n");
+    printf("3. Добавление в файл \n");
+    printf("4. Выход \n");
     scanf("%d" , &variant);
     return variant;
 }
 
 int appendTask(FILE *file)
 {
-	int idCount = 0;	
-	char ch = 'n';
-	file = fopen("task.dat", "ab");
-	printf("\e[2J\e[H");
-	printf("==========NEW FILE MODE============\n\n");
+    int idCount = 0;	
+    char ch = 'n';
+    file = fopen("task.dat", "ab");
+    printf("\e[2J\e[H");
+    printf("==========NEW FILE MODE============\n\n");
 	
-	do {
-		task.id = ++idCount;
-		printf("TASK TITLE: ");
-		fgets(task.tTitle, 30, stdin);
-		printf("TASK DESCRIPTION:");
-        	fgets(task.tInfo, 100, stdin);
+    do {
+        task.id = ++idCount;
+        printf("TASK TITLE: ");
+        fgets(task.tTitle, 30, stdin);
+        printf("TASK DESCRIPTION:");
+      	fgets(task.tInfo, 100, stdin);
 		
-		do {
-			printf("\nIs it important? [y/n]: ");
-            		scanf("%c%*c", &ch);;
-            		if (ch == 'y') {
-                		task.isImportant = 1;
-            		} else task.isImportant = 0;
-    		} while (ch != 'y' && ch != 'n');
+	do {
+            printf("\nIs it important? [y/n]: ");
+            scanf("%c%*c", &ch);;
+            if (ch == 'y') {
+            task.isImportant = 1;
+            } else task.isImportant = 0;
+    	} while (ch != 'y' && ch != 'n');
 
-		time_t t = time(NULL);
-		struct tm tm = *localtime(&t);
-		sprintf(task.tAcceptedDate, "%d-%d-%d %d:%d:%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+	time_t t = time(NULL);
+	struct tm tm = *localtime(&t);
+	sprintf(task.tAcceptedDate, "%d-%d-%d %d:%d:%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
     
-		task.isInProgress = 1;
+	task.isInProgress = 1;
         
-        	fwrite(&task, sizeof(task), 1, file);
-        	printf("Are we done? [y/n]: ");
-		scanf("%c%*c", &ch);
-	} while (ch != 'y');
+        fwrite(&task, sizeof(task), 1, file);
+        printf("Are we done? [y/n]: ");
+	scanf("%c%*c", &ch);
+    } while (ch != 'y');
     
-	fclose(file);
+    fclose(file);
 }		
