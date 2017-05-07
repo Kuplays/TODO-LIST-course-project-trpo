0dcf009f5e7ccb6fbe1f18fe22bea280ba56db8f
crasher746 <Crasher746@gmail.com>
2017-05-08 02:20:39 +0700

    Change prototype newFile function

--- includes/task.h
+++ includes/task.h
@@ -11,7 +11,7 @@ struct Task {
 } task;
 
 //function prototypes goes here
-int newFile(FILE *);
+int newFile(FILE *, char*);
 void printAllTasks(FILE *);
 void showTask(FILE *, int); //probably by id?
 int removeTask(FILE *, int);//same as above?
