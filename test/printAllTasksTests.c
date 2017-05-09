3a0c71d4cfb1536ab1048ea1da5fdb3f93bc98f6
Aleksandr Kuplays <alexkuplays@gmail.com>
2017-05-09 13:03:51 +0700

    Added test coverage for printTasks

--- test/printAllTasksTests.c
+++ test/printAllTasksTests.c
@@ -1,2 +1,19 @@
 #include "task.h"
 #include "ctest.h"
+
+void printAllTasksTest(FILE *file)
+{
+    file = fopen("tasks.dat", "rb");
+    fread(&task, sizeof(task), 1, file);
+    
+    
+    
+    fclose(file);
+}
+
+CTEST(PRINT_TASKS_TESTS, NULL_POINTER_OK)
+{
+	FILE *file;
+	printAllTasksTest(file);
+	ASSERT_NOT_NULL(file);
+}
