91cd7a35a6328d1e8e0f245a9637515b02167dc6
Aleksandr Kuplays <alexkuplays@gmail.com>
2017-05-13 01:00:51 +0700

    Add tests for fileExists function

--- test/helpersTests.c
+++ test/helpersTests.c
@@ -1,3 +1,14 @@
 #include "task.h"
 #include "ctest.h"
 
+CTEST(FILE_EXIST_FUNCTION, fileExistsOK)
+{
+	int result = file_exist("tasksTest.dat");
+	ASSERT_EQUAL(1, result);
+}
+
+CTEST(FILE_EXIST_FUNCTION, fileExistsFAIL)
+{
+	int result = file_exist("no_such_file.dat");
+	ASSERT_EQUAL(0, result);
+}
