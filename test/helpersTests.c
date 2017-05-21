6cb99eba06f9fe8f12e3b75401cfc936a983d4e2
Aleksandr Kuplays <alexkuplays@gmail.com>
2017-05-21 13:14:58 +0700

    Added tests for getSize function

--- test/helpersTests.c
+++ test/helpersTests.c
@@ -31,3 +31,36 @@ CTEST(READ_FROM_FILE_TESTS, fileNotPresentOK) {
 	int result = readFromFile(t, "dummyFile.dat");
 	ASSERT_EQUAL(-1, result);
 }
+
+CTEST(GET_SIZE_TESTS, fileNotPresentOK) {
+	int result = getSize("dummyFile.dat");
+	ASSERT_EQUAL(-1, result);
+}
+
+CTEST(GET_SIZE_TESTS, elementsCount_2_OK) {
+	FILE *f = fopen("testGetSize.dat", "wb");
+	task.id = 100;
+	fwrite(&task, sizeof(task), 1, f);
+	task.id = 200;
+	fwrite(&task, sizeof(task), 1, f);
+	fclose(f);
+	int result = getSize("testGetSize.dat");
+	ASSERT_EQUAL(2, result);
+}
+
+CTEST(GET_SIZE_TESTS, elementsCount_10_OK) {
+	FILE *f = fopen("testGetSize.dat", "wb");
+	int count = 0;
+
+	do {
+		task.id = 1;
+		fwrite(&task, sizeof(task), 1, f);
+		count ++;
+	} while (count < 10);
+
+	fclose(f);
+	int result = getSize("testGetSize.dat");
+	ASSERT_EQUAL(10, result);
+
+
+}
