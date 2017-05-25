e70c31a6242176bf436163d9cf4f2de76e3353e1
Aleksandra <sorokinaa25@gmail.com>
2017-05-25 21:04:23 +0700

    Deleted useless commented code

--- src/main.c
+++ src/main.c
@@ -25,49 +25,5 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
-	/*struct Task task;
-	gatherInput(&task);
-	writeToFile(task);
-	printAllTasks();
-*/
-	//gatherInput(&tsk);
-	//writeToFile(tsk);
-	/*int size = getSize("tasks.dat"); //1
-	struct Task tasks[size];
-
-	readFromFile(tasks, "tasks.dat");
-
-	for (int i = 0; i < size; i++)
-	{
-		printf("ID: %d\nTITLE: %s\nDESCR: %s", tasks[i].id, tasks[i].tTitle, tasks[i].tInfo);
-		printf("\n======\n");
-	}*/
-  /*int variant;
-  FILE *file;
-  while(1) {
-  	variant = menu ();
-  	switch(variant) {
-  	case 1: 
-  		if (file_exist("tasks.dat"))
-  		{
-  			printf("EXIST!");
-  			newEntry(file, "ab", countID(file, "tasks.dat"));
-  		} else {
-  			printf("NEW FILE MODE!");
-  			newEntry(file, "wb", 0);
-  		}
-		break;
-  	case 2:
-		printAllTasks(file);
-		break;
-        case 3:
-		printImportantTasks(file);
-		break;
-  	case 4:
-		return EXIT_SUCCESS;
-		break;
-    	}
-  }*/
-
   return 0;
 }
