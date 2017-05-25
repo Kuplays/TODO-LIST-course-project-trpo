cf8d9a825fbef66ee79ebd67a8942520e5499339
Aleksandra <sorokinaa25@gmail.com>
2017-05-25 19:59:55 +0700

    Added infinite menu loop

--- src/main.c
+++ src/main.c
@@ -4,7 +4,44 @@
 
 int main(int argc, char *argv[])
 {
-  int variant;
+	int choice;
+	struct Task task;
+	while(1) {
+		showMenu();
+		printf("\n\nTHE CHOISE IS YOURS: ");
+		scanf("%d%*c", &choice);
+
+		switch(choice) {
+			case 1:
+			gatherInput(&task);
+			writeToFile(task);
+			break;
+			case 2:
+			printAllTasks();
+			case 4:
+			printf("\033c");
+			return EXIT_SUCCESS;
+			break;
+		}
+	}
+	/*struct Task task;
+	gatherInput(&task);
+	writeToFile(task);
+	printAllTasks();
+*/
+	//gatherInput(&tsk);
+	//writeToFile(tsk);
+	/*int size = getSize("tasks.dat"); //1
+	struct Task tasks[size];
+
+	readFromFile(tasks, "tasks.dat");
+
+	for (int i = 0; i < size; i++)
+	{
+		printf("ID: %d\nTITLE: %s\nDESCR: %s", tasks[i].id, tasks[i].tTitle, tasks[i].tInfo);
+		printf("\n======\n");
+	}*/
+  /*int variant;
   FILE *file;
   while(1) {
   	variant = menu ();
@@ -29,7 +66,7 @@ int main(int argc, char *argv[])
 		return EXIT_SUCCESS;
 		break;
     	}
-  }
+  }*/
 
   return 0;
 }
